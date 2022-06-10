b91c45c8ceb6cd44d5c44a249cd9491529ddd289
ezcafe <ezcafe@outlook.com>
2022-06-10 21:07:11 +0700

    duplicati update script

--- duplicati/before.sh
+++ duplicati/before.sh
@@ -3,7 +3,7 @@
 # https://github.com/duplicati/duplicati/blob/master/Duplicati/Library/Modules/Builtin/run-script-example.sh
 
 echo "stop all Docker containers except duplicati"
-docker stop $(docker ps -aq | grep -v $(docker ps -a -q --filter="name=duplicati"))
+docker stop $(docker ps -aq | grep -v $(docker ps -aq --filter="name=duplicati"))
 
 # We want the exit code to always report success.
 # For scripts that can abort execution, use the option
