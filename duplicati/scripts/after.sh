98537164ec2f6d6e79bd2d581dae714cff38c18a
ezcafe <ezcafe@outlook.com>
2022-06-26 11:30:08 +0700

    duplicati update scripts

--- duplicati/scripts/after.sh
+++ duplicati/scripts/after.sh
@@ -2,8 +2,15 @@
 
 # https://github.com/duplicati/duplicati/blob/master/Duplicati/Library/Modules/Builtin/run-script-example.sh
 
-echo "restart all exited Docker containers"
-docker start $(docker ps -aq --filter status=exited)
+OPERATIONNAME=$DUPLICATI__OPERATIONNAME
+ 	
+if [ "$OPERATIONNAME" == "Restore" ]
+then
+    echo "restart all exited Docker containers"
+    docker start $(docker ps -aq --filter status=exited)
+else
+    exit 0
+fi
 
 # We want the exit code to always report success.
 # For scripts that can abort execution, use the option
--- duplicati/scripts/before.sh
+++ duplicati/scripts/before.sh
@@ -2,8 +2,15 @@
 
 # https://github.com/duplicati/duplicati/blob/master/Duplicati/Library/Modules/Builtin/run-script-example.sh
 
-echo "stop all Docker containers except duplicati"
-docker stop $(docker ps -aq | grep -v $(docker ps -aq --filter="name=duplicati"))
+OPERATIONNAME=$DUPLICATI__OPERATIONNAME
+ 	
+if [ "$OPERATIONNAME" == "Backup" ]
+then
+    echo "stop all Docker containers except duplicati"
+    docker stop $(docker ps -aq | grep -v $(docker ps -aq --filter="name=duplicati"))
+else
+    exit 0
+fi
 
 # We want the exit code to always report success.
 # For scripts that can abort execution, use the option
